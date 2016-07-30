36c9eac62b29283ef4821b37cc5f46b7e004fb47
yishuixm <1830802211@qq.com>
2016-07-30 13:35:34 +0800

    20160730

---
+++ src/Ht3g.php
@@ -0,0 +1,57 @@
+<?php
+
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/30
+ * Time: 13:20
+ */
+
+namespace yishuixm\sms;
+
+class Ht3g
+{
+    private $server_url = 'http://sms.ht3g.com/sms.aspx';
+    //企业ID
+    private $userid;
+    //用户帐号，由系统管理员
+    private $account;
+    //用户账号对应的密码
+    private $password;
+
+    /**
+     * 构造函数
+     * @param $userid 企业ID
+     * @param $account 用户帐号
+     * @param $password 用户账号对应的密码
+     */
+    public function __construct($userid,$account,$password)
+    {
+        $this->userid = $userid;
+        $this->account = $account;
+        $this->password = $password;
+    }
+
+    public function sendTo($mobile, $content, $sendTime=''){
+        $data = http_build_query([
+            'userid'        => $this->userid,
+            'account'       => $this->account,
+            'password'      => $this->password,
+            'mobile'        => $mobile,
+            'content'       => mb_convert_encoding($content, 'utf-8', 'auto'),
+            'action'        => 'send',
+            'sendTime'      => $sendTime,
+            'extno'         => ''
+        ]);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->server_url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'mypost=' . $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, "somorn.com's CURL Example beta V1.0");
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
+    }
+}
